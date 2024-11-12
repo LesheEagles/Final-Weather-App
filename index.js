fb41fa55d34d35310033cbc0434a7a6bc0284e54
LesheEagles <litches87@gmail.com>
2024-11-12 19:31:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -81,4 +81,4 @@ function displayForecast(response) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Paris"); // Default city
+searchCity("Paris"); 
